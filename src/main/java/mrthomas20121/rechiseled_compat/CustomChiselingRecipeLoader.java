3c79166fb81b18184bbf79b0a3a0330c9602122e
Mrthomas20121 <thomas77300@gmail.com>
2022-04-22 16:00:04 +0200

    Test

--- src/main/java/mrthomas20121/rechiseled_compat/CustomChiselingRecipeLoader.java
+++ src/main/java/mrthomas20121/rechiseled_compat/CustomChiselingRecipeLoader.java
@@ -4,7 +4,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.supermartijn642.rechiseled.chiseling.ChiselingRecipe;
 import com.supermartijn642.rechiseled.chiseling.ChiselingRecipeLoader;
+import com.supermartijn642.rechiseled.chiseling.ChiselingRecipes;
 import net.minecraft.client.resources.JsonReloadListener;
 import net.minecraft.profiler.IProfiler;
 import net.minecraft.resources.IResourceManager;
@@ -14,37 +16,45 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.commons.lang3.reflect.FieldUtils;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.util.*;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE, modid = RechiseledCompat.MOD_ID)
-public class CustomChiselingRecipeLoader extends ChiselingRecipeLoader {
+public class CustomChiselingRecipeLoader extends JsonReloadListener {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onAddReloadListener(AddReloadListenerEvent e){
         e.addListener(new CustomChiselingRecipeLoader());
     }
 
+    private static final Gson GSON = new GsonBuilder().setLenient().create();
+
     public CustomChiselingRecipeLoader(){
-        super();
+        super(GSON, "custom_chiseling_recipes");
     }
 
     @Override
     protected void apply(@Nonnull Map<ResourceLocation, JsonElement> entries, @Nonnull IResourceManager resourceManager, @Nonnull IProfiler profilerFiller) {
-        Map<ResourceLocation, JsonElement> copy = new HashMap<>();
+        List<ChiselingRecipe> recipes = new ArrayList<>();
         for(Map.Entry<ResourceLocation,JsonElement> entry : entries.entrySet()){
             JsonObject object = entry.getValue().getAsJsonObject();
             if(object.has("mod_id")) {
                 if(ModList.get().isLoaded(object.get("mod_id").getAsString())) {
-                    copy.put(entry.getKey(), entry.getValue());
+                    recipes.add(ChiselingRecipe.Serializer.fromJson(entry.getKey(), object));
                 }
             }
-            else {
-                copy.put(entry.getKey(), entry.getValue());
-            }
         }
-        super.apply(copy, resourceManager, profilerFiller);
+        recipes.addAll(ChiselingRecipes.getAllRecipes());
+        try {
+            Field field = ChiselingRecipes.class.getDeclaredField("chiselingRecipes");
+            field.setAccessible(true);
+            List<ChiselingRecipe> recipeList = (List<ChiselingRecipe>) field.get(null);
+            recipeList = Collections.unmodifiableList(recipes);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
